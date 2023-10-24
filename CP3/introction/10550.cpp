a5faccce3f4f7f1a77fcc1039bc5003ac4d49e92
JFY1104 <jack31304@gmail.com>
2023-10-24 10:33:01 +0800

    1

--- CP3/introction/10550.cpp
+++ CP3/introction/10550.cpp
@@ -1,14 +1,54 @@
-#include<iostream>
-#include<string>
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <vector>
 using namespace std;
 
-int main(){
+int main()
+{
     string str;
-    while (getline(cin,str))
+    stringstream s1;
+    while (getline(cin, str))
     {
+        int degree = 0;
+        int process = 0;
+        vector<int> v;
         if (str == "0 0 0 0")
             break;
-        cout << str[0] << str[] <<endl;
+        s1 << str;
+        while (s1 >> process)
+        {
+            v.push_back(process);
+        }
+
+        if (v[0] < v[1])
+        {
+            degree += (v[0] - v[1] + 40) * 9;
+        }
+        else
+        {
+            degree += (v[0] - v[1]) * 9;
+        }
+        if (v[1] < v[2])
+        {
+            degree += (v[2] - v[1]) * 9;
+        }
+        else
+        {
+            degree += (v[2] - v[1] + 40) * 9;
+        }
+        if (v[2] < v[3])
+        {
+            degree += (v[2] - v[3] + 40) * 9;
+        }
+        else
+        {
+            degree += (v[2] - v[3]) * 9;
+        }
+        degree += 1080;
+        cout << degree << endl;
+        v.clear();
+        s1.clear();
+        s1.str("");
     }
-    
 }
